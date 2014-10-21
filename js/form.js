b0717c71420d1d032ad26832a2e0e2669c80bcaf
Nicolas Deverge <ndeverge@ekito.fr>
2014-10-21 09:09:39 +0200

    Google Form pour récolter les emails

--- js/form.js
+++ js/form.js
@@ -4,9 +4,9 @@ function doSubmit() {
     var email = $("#email").val();
 
     $.ajax({
-        url:"https://docs.google.com/forms/d/whatever/formResponse",
+        url:"https://docs.google.com/forms/d/1ehKjRrixCVh5oYXpECAGppMArw-ZQLIEV9gq6-5y0QE/formResponse",
         data: {
-            'entry.462923373': email
+            'entry.474034136': email
         },
         type: "POST",
         dataType: "xml",
